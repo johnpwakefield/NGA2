5536f163c93c96c06f99234ad0e95f7e4871bec0
John Wakefield <jwake@umich.edu>
2023-04-24 04:29:42 -0400

    resolved naming / merge issues

--- src/grid/iterator_class.f90
+++ src/grid/iterator_class.f90
@@ -8,10 +8,10 @@ module iterator_class
    use mpi_f08,     only: MPI_Comm
    implicit none
    private
-   
+
    ! Expose type/constructor/methods
-   public :: iterator, locator_gen_ftype
-   
+   public :: iterator, locator_ftype
+
    !> itr object definition
    type :: iterator
       ! Parallel info for the iterator
@@ -35,18 +35,18 @@ module iterator_class
 
    !> iterator generating function type
    interface
-     logical function locator_gen_ftype(pargrid,ind1,ind2,ind3)
+     logical function locator_ftype(pargrid,ind1,ind2,ind3)
        use pgrid_class, only: pgrid
        class(pgrid), intent(in) :: pargrid
        integer, intent(in) :: ind1,ind2,ind3
-     end function locator_gen_ftype
+     end function locator_ftype
    end interface
-   
+
    !> Declare single-grid iterator constructor
    interface iterator
       procedure construct_from_function
    end interface iterator
-   
+
 contains
 
    !> Iterator constructor from a "locator" tester function
@@ -58,19 +58,19 @@ contains
       type(iterator) :: self
       class(pgrid), target, intent(in) :: pg
       character(len=*), intent(in) :: name
-      procedure(locator_gen_ftype) :: locator
+      procedure(locator_ftype) :: locator
       character(len=1), optional :: face
       integer :: i,j,k,cnt
       integer :: color,key,ierr
       integer :: f_imax_,f_jmax_,f_kmax_
       integer :: f_imin_,f_jmin_,f_kmin_
-      
+
       ! Set the name for the iterator
       self%name=trim(adjustl(name))
-      
+
       ! Point to pgrid object
       self%pg=>pg
-      
+
       ! Check if a face has been specified
       self%face=' '
       if (present(face)) then
@@ -82,17 +82,17 @@ contains
          case default; call die('[iterator constructor] Unknown face value - if provided, expecting x, y, or z')
          end select
       end if
-      
+
       ! Modify iteration upper bounds to include faces at boundary and cells just past boundary
       f_imax_=self%pg%imax_; if (.not.self%pg%xper.and.self%pg%iproc.eq.self%pg%npx.and.(self%face.eq.'c'.or.self%face.eq.'x')) f_imax_=f_imax_+1
       f_jmax_=self%pg%jmax_; if (.not.self%pg%yper.and.self%pg%jproc.eq.self%pg%npy.and.(self%face.eq.'c'.or.self%face.eq.'y')) f_jmax_=f_jmax_+1
       f_kmax_=self%pg%kmax_; if (.not.self%pg%zper.and.self%pg%kproc.eq.self%pg%npz.and.(self%face.eq.'c'.or.self%face.eq.'z')) f_kmax_=f_kmax_+1
-      
+
       ! Modify iteration lower bounds to include cells just past boundary, faces remain at boundary
       f_imin_=self%pg%imin_; if (.not.self%pg%xper.and.self%pg%iproc.eq.1.and.self%face.eq.'c') f_imin_=f_imin_-1
       f_jmin_=self%pg%jmin_; if (.not.self%pg%yper.and.self%pg%jproc.eq.1.and.self%face.eq.'c') f_jmin_=f_jmin_-1
       f_kmin_=self%pg%kmin_; if (.not.self%pg%zper.and.self%pg%kproc.eq.1.and.self%face.eq.'c') f_kmin_=f_kmin_-1
-      
+
       ! Loop over local domain with overlap to count iterator cells
       self%n_=0; self%no_=0
       do k=self%pg%kmino_,self%pg%kmaxo_
@@ -107,7 +107,7 @@ contains
             end do
          end do
       end do
-      
+
       ! Create sub-communicator
       if (self%no_.eq.0) then
          color=MPI_UNDEFINED
@@ -119,7 +119,7 @@ contains
          self%amIn=.true.
       end if
       call MPI_COMM_SPLIT(self%pg%comm,color,key,self%comm,ierr)
-      
+
       ! If we are not in the iterator, we are done
       if (.not.self%amIn) then
          self%nproc=0
@@ -127,12 +127,12 @@ contains
          self%amRoot=.false.
          return
       end if
-      
+
       ! Get nproc, rank, and root
       call MPI_COMM_SIZE(self%comm,self%nproc,ierr)
       call MPI_COMM_RANK(self%comm,self%rank,ierr)
       self%amRoot=(self%rank.eq.0)
-      
+
       ! Create unstructured mapping to iterator cells - first inside cells then overlap
       allocate(self%map(1:3,1:self%no_))
       cnt=0
@@ -161,9 +161,9 @@ contains
             end do
          end do
       end do
-      
+
    end function construct_from_function
-   
+
    !> Basic printing of iterator
    subroutine iterator_print(this)
       use, intrinsic :: iso_fortran_env, only: output_unit
@@ -171,10 +171,10 @@ contains
       implicit none
       class(iterator), intent(in) :: this
       integer :: i,ierr,ntot,maxi,mini,maxj,minj,maxk,mink
-      
+
       ! Return if not in
       if (.not.this%amIn) return
-      
+
       ! Do some processing on the iterator
       call MPI_ALLREDUCE(       this%n_       ,ntot,1,MPI_INTEGER,MPI_SUM,this%comm,ierr)
       call MPI_ALLREDUCE(minval(this%map(1,:)),mini,1,MPI_INTEGER,MPI_MIN,this%comm,ierr)
@@ -183,7 +183,7 @@ contains
       call MPI_ALLREDUCE(maxval(this%map(2,:)),maxj,1,MPI_INTEGER,MPI_MAX,this%comm,ierr)
       call MPI_ALLREDUCE(minval(this%map(3,:)),mink,1,MPI_INTEGER,MPI_MIN,this%comm,ierr)
       call MPI_ALLREDUCE(maxval(this%map(3,:)),maxk,1,MPI_INTEGER,MPI_MAX,this%comm,ierr)
-      
+
       ! Output
       if (this%amRoot) then
          write(output_unit,'("Iterator [",a,"] for pgrid [",a,"] ")') trim(this%name),trim(this%pg%name)
@@ -200,5 +200,5 @@ contains
          end if
       end do
    end subroutine iterator_print
-   
+
 end module iterator_class
